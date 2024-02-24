382dd897fab6c07c533a518a78ba41583419277f
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-02-24 11:32:23 +0000

    Updated RegForm

--- src/components/Employee/RegForm.jsx
+++ src/components/Employee/RegForm.jsx
@@ -1,37 +1,18 @@
-import React from 'react'
+ import React from 'react';
 
 function RegForm() {
   return (
-<div>
-<h1>Registration Form</h1>
-<form action="/register" method='post'>
-  <label htmlFor="usename">Username</label>
-  <input type="text" name="username" id="username"/>
-  
-</form>
-</div>
-
-  )
+    <div>
+      <h1>Registration Form</h1>
+      <form action="/register" method="post">
+        <label htmlFor="username">Username: </label>
+        <input type="text" name="username" id="username" /><br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" /><br />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 }
 
-export default RegForm
-
-
-// import React from 'react';
-
-// function RegForm() {
-//   return (
-//     <div>
-//       <h1>Registration Form</h1>
-//       <form action="/register" method="post">
-//         <label htmlFor="username">Username: </label>
-//         <input type="text" name="username" id="username" /><br />
-//         <label htmlFor="password">Password: </label>
-//         <input type="password" name="password" id="password" /><br />
-//         <input type="submit" value="Register" />
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default RegForm;
+export default RegForm;
