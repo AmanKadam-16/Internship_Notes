6ebcbc3fbf25e24ad050418ef86d275fed77732b
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-03-07 08:24:50 +0000

    hjk

--- src/requests/SchoolService/schoolServices.ts
+++ src/requests/SchoolService/schoolServices.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://localhost:44349/',
+  baseURL: 'http://employeeapi.aaditechnology.info/',
   headers: {
     'Content-type': 'application/json',
     'Access-Control-Allow-Origin': '*'
